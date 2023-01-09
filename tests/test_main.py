c1a845e8d7a8e58f16c50ee072a9f181f420598b
Daniel <d_karavaev2@mail.ru>
2023-01-09 23:26:26 +0300

    Add tests to Github Actions

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,33 +1,33 @@
 import requests
 
-api_url = 'http://localhost:8000'
+api_url = 'http://localhost:8001'
 
 def test_healthcheck():
     response = requests.get(f'{api_url}/__health')
     assert response.status_code == 200
 
-class TestAuthor():
-    def test_get_empty_auth(self):
+class TestAuthors():
+    def test_get_empty_authors(self):
         response = requests.get(f'{api_url}/v1/auth')
         assert response.status_code == 200
         assert len(response.json()) == 0
 
-    def test_add_author(self):
-        body = { "title": "New author", "body": "genre" }
+    def test_create_authors(self):
+        body = { "nickname": "nickname", "genre": "genre" }
         response = requests.post(f'{api_url}/v1/auth', json = body)
         assert response.status_code == 200
-        assert response.json().get('title') == 'New author'
-        assert response.json().get('body') == 'genre'
-        assert response.json().get('id') == 0
-
-    def get_auth_by_id(self):
-        response = requests.get(f'{api_url}/v1/auth/0')
-        assert response.status_code == 200
-        assert response.json().get('title') == 'New author'
+        assert response.json().get('nickname') == 'nickname'
         assert response.json().get('genre') == 'genre'
         assert response.json().get('id') == 0
 
-    def test_get_not_empty_author(self):
+    def test_get_authors_by_id(self):
+        response = requests.get(f'{api_url}/v1/auth/0')
+        assert response.status_code == 200
+        assert response.json().get('nickname') == 'nickname'
+        assert response.json().get('genre') == 'genre'
+        assert response.json().get('id') == 0
+
+    def test_get_not_empty_authors(self):
         response = requests.get(f'{api_url}/v1/auth')
         assert response.status_code == 200
         assert len(response.json()) == 1
